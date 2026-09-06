fc1b959fa8938f76e02eedb04a3866a5253c7a76
Amp Accessible <amp-accessible@local.invalid>
2026-09-06 14:55:02 -0300

    Amp Accessible 2.41.62 - prueba actualizador automatico JAWS

--- AppInfo.cs
+++ AppInfo.cs
@@ -2,8 +2,8 @@ namespace GDMAmpAccessible;
 
 internal static class AppInfo
 {
-    public const string Version = "2.41.61";
-    public const string BuildName = "Actualizador Reinicio Seguro JAWS";
+    public const string Version = "2.41.62";
+    public const string BuildName = "Prueba Actualizacion Automatica JAWS";
     public static string WindowTitle => $"Amp Accessible {Version}";
     public static string DiagnosticTitle => $"Amp Accessible {Version} - diagnóstico accesible";
 }
--- Installer/AmpAccessible.iss
+++ Installer/AmpAccessible.iss
@@ -1,5 +1,5 @@
 #define MyAppName "Amp Accessible"
-#define MyAppVersion "2.41.61"
+#define MyAppVersion "2.41.62"
 #define MyAppPublisher "GDM"
 #define MyAppExeName "AmpAccessible.exe"
 
