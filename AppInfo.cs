3eebcfcbccbaeef002a2104ac6c97e1412f04474
Amp Accessible <amp-accessible@local.invalid>
2026-09-06 11:11:54 -0300

    Amp Accessible 2.41.60 - reinicio actualizador blindado JAWS

--- AppInfo.cs
+++ AppInfo.cs
@@ -2,8 +2,8 @@ namespace GDMAmpAccessible;
 
 internal static class AppInfo
 {
-    public const string Version = "2.41.58";
-    public const string BuildName = "Actualizaciones GitHub Automaticas JAWS";
+    public const string Version = "2.41.60";
+    public const string BuildName = "Reinicio Actualizador Blindado JAWS";
     public static string WindowTitle => $"Amp Accessible {Version}";
     public static string DiagnosticTitle => $"Amp Accessible {Version} - diagnóstico accesible";
 }
