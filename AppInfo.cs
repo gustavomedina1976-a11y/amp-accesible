a2eda3364f52e7317935c22a134ea74200345e59
Amp Accessible <amp-accessible@local.invalid>
2026-09-16 15:39:16 -0300

    Amp Accessible 2.41.76 - Steve Vai Legacy Lead Cremoso JAWS

--- AppInfo.cs
+++ AppInfo.cs
@@ -2,8 +2,8 @@ namespace GDMAmpAccessible;
 
 internal static class AppInfo
 {
-    public const string Version = "2.41.75";
-    public const string BuildName = "Nivelacion Clean y Actualizacion Rapida JAWS";
+    public const string Version = "2.41.76";
+    public const string BuildName = "Steve Vai Legacy Lead Cremoso JAWS";
     public static string WindowTitle => $"Amp Accessible {Version}";
     public static string DiagnosticTitle => $"Amp Accessible {Version} - diagnóstico accesible";
 }
